73b0f48aa9ed33f181c6bfc028537818b6518065
dam2217 <dam2217@ic.ac.uk>
2019-11-25 16:55:17 +0000

    Updating the changes Dimitra made on Monday Nov 25th

---
+++ app/src/main/java/com/example/eczema_app/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,16 @@
+package com.example.eczema_app.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Notifications");
+    }
+    public  LiveData<String> getText() {return mText;}
+}

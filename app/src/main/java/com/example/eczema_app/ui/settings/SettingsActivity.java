03c3f2188f288449919fa7faaa7111717bc55db2
Megan Allerton <45626633+ma6417@users.noreply.github.com>
2019-12-22 23:49:28 +0000

    trying location

--- app/src/main/java/com/example/eczema_app/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/eczema_app/ui/settings/SettingsActivity.java
@@ -1,14 +1,166 @@
 package com.example.eczema_app.ui.settings;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.example.eczema_app.MainActivity;
 import com.example.eczema_app.R;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class SettingsActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState){
+//code adapted from https://developer.android.com/guide/topics/ui/settings
+
+public class SettingsActivity extends AppCompatActivity{
+//
+//    private String lat = "lllll";
+//    private String lon;
+//    private FusedLocationProviderClient mFusedLocationClient;
+//    private int PERMISSION_ID = 44;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        System.out.println(lat);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_settings);
+//        System.out.println(lat);
+
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//        getLastLocation();
+//        System.out.println(lat);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_settings, new SettingsFragment())
+                .commit();
+
     }
+
+//
+//    @SuppressLint("MissingPermission")
+//    private void getLastLocation(){
+//        if (checkPermissions()) {
+//            if (isLocationEnabled()) {
+//                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+//                        new OnCompleteListener<Location>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<Location> task) {
+//                                Location location = task.getResult();
+//                                if (location == null) {
+//                                    requestNewLocationData();
+//                                    System.out.println("bbbb");
+//                                } else {
+//                                    lat = Double.toString(location.getLatitude());
+//                                    System.out.println(lat);
+//                                    System.out.println("cccccc");
+//                                    lon = Double.toString(location.getLongitude());
+//                                }
+//                            }
+//                        }
+//                );
+//            } else {
+//                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivity(intent);
+//            }
+//        } else {
+//            requestPermissions();
+//        }
+//    }
+//
+//    @SuppressLint("MissingPermission")
+//    private void requestNewLocationData(){
+//
+//        LocationRequest mLocationRequest = new LocationRequest();
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        mLocationRequest.setInterval(0);
+//        mLocationRequest.setFastestInterval(0);
+//        mLocationRequest.setNumUpdates(1);
+//
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//        mFusedLocationClient.requestLocationUpdates(
+//                mLocationRequest, mLocationCallback,
+//                Looper.myLooper()
+//        );
+//
+//    }
+//
+//    private LocationCallback mLocationCallback = new LocationCallback() {
+//        @Override
+//        public void onLocationResult(LocationResult locationResult) {
+//            Location mLastLocation = locationResult.getLastLocation();
+//            lat = Double.toString(mLastLocation.getLatitude());
+//            lon = Double.toString(mLastLocation.getLongitude());
+//        }
+//    };
+//
+//    private boolean checkPermissions() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+//                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    private void requestPermissions() {
+//        ActivityCompat.requestPermissions(
+//                this,
+//                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+//                PERMISSION_ID
+//        );
+//    }
+//
+//    private boolean isLocationEnabled() {
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+//                LocationManager.NETWORK_PROVIDER
+//        );
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == PERMISSION_ID) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                getLastLocation();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onResume(){
+//        super.onResume();
+//        if (checkPermissions()) {
+//            getLastLocation();
+//        }
+//
+//    }
+
 }
+
+
+

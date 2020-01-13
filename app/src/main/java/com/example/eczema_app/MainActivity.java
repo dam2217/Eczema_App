5b04be4ebf28ff33935057a145d6a9ada8a9ea9d
Megan Allerton <45626633+ma6417@users.noreply.github.com>
2020-01-13 00:05:57 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/eczema_app/MainActivity.java
+++ app/src/main/java/com/example/eczema_app/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.eczema_app;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 
+
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,23 +21,46 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
+import com.example.eczema_app.ui.home.LoggedDataEntry;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    SharedPreferences sharedPreferences;
+    NavigationView navigationView;
+    View hview;
+    TextView personName;
+    TextView personEmail;
+    List<LoggedDataEntry> logList = new ArrayList<LoggedDataEntry>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        logList = getIntent().getParcelableExtra("logList");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
+        hview = navigationView.getHeaderView(0);
+        personName = hview.findViewById(R.id.personName);
+        personEmail = hview.findViewById(R.id.email);
+
+
+
+
+
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -38,8 +70,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-//      Reminder Time of 8pm - could eventually be user input
+        
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 20);
         calendar.set(Calendar.MINUTE, 00);
@@ -55,6 +86,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        final AlertDialog inflammationLocations = new AlertDialog.Builder(this, R.style.CustomDialogTheme).create();
+        inflammationLocations.setTitle("Inflammations");
+        inflammationLocations.setMessage("Insert list of locations and severity ");
+//        show on click of locations button
+//        inflammationLocations.show();
+
+        final AlertDialog treatments = new AlertDialog.Builder(this, R.style.CustomDialogTheme).create();
+        treatments.setTitle("Treatment: **Name of Treatment**");
+        treatments.setMessage("Insert list of where treatment used");
+//        treatments.show();
+
+        final AlertDialog notes = new AlertDialog.Builder(this, R.style.CustomDialogTheme).create();
+        notes.setTitle("Notes");
+        notes.setMessage("Insert Notes");
+//        notes.show();
+        
     }
 
     @Override
@@ -63,7 +111,45 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+//
+//    protected void createLocationRequest() {
+//        LocationRequest locationRequest = LocationRequest.create();
+//        locationRequest.setInterval(10000);
+//        locationRequest.setFastestInterval(5000);
+//        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+//    }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        String userName = new String();
+        String emailAddress = new String();
+
+        navigationView = findViewById(R.id.nav_view);
+        hview = navigationView.getHeaderView(0);
+        personName = hview.findViewById(R.id.personName);
+        personEmail = hview.findViewById(R.id.email);
+
+        userName = sharedPreferences.getString("name","");
+        emailAddress = sharedPreferences.getString("email", "");
+//        Bundle extras = getIntent().getExtras();
+//        if(extras !=null) {
+//            userName = extras.getString("KEY");
+//        }
+
+        personName.setText(userName);
+        personEmail.setText(emailAddress);
+
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences prefs,
+                                          String rootKey) {
+        final TextView personName = findViewById(R.id.personName);
+        final TextView personEmail = findViewById(R.id.email);
+        personEmail.setText(sharedPreferences.getString("email", ""));
+        personName.setText(sharedPreferences.getString("name", ""));
+    }
 
 
 }

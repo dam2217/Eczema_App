3d8828de1f7c7a0afa5265c848c27d2e21034ac1
Megan Allerton <45626633+ma6417@users.noreply.github.com>
2020-01-13 00:08:28 +0000

    Create Profile.java

---
+++ app/src/main/java/com/example/eczema_app/ui/settings/Profile.java
@@ -0,0 +1,17 @@
+package com.example.eczema_app.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eczema_app.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+
+    }
+}
